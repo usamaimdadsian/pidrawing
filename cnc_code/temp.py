b3cec33c4cc6002bc8c07375ccaae80853ca905e
usamaimdadsian <usamaimdadsian@gmail.com>
2021-06-11 15:52:54 +0100

    Calibration Done

--- cnc_code/temp.py
+++ cnc_code/temp.py
@@ -2,10 +2,17 @@ import time
 from Motor import Motor
 
 if __name__ == '__main__':
-    m1 = Motor(17,27) #11,13 
+    # m1 = Motor(17,27) #11,13 
     m2 = Motor(22,23) #15,16
 
 
+
     while True:
-        m1.move(clockwise=False,steps=200)
-        m2.move(True,200)
+        inp = input(":")
+        if inp == "q":
+            break
+        inp = int(inp)
+        dire = (inp > 0)
+        # m1.move(dire,abs(inp))
+        m2.move(dire,abs(inp))
+
