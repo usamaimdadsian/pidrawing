867a7140f8ee0c0d306092b8cb2e0d9821854d2c
usamaimdad <rreigns273@gmail.com>
2021-06-12 22:44:58 +0500

    Modified the drawing algorithm

--- Drawing.py
+++ Drawing.py
@@ -1,3 +1,5 @@
+import math
+import numpy as np
 from Controller import Controller
 class Drawing:
     def __init__(self,scene,board):
@@ -7,31 +9,60 @@ class Drawing:
 
 
     def startDrawing(self):
-        self.left = True
-        for i in range(len(self.board)):
-            if self.left:
-                for j in range(len(self.board[i])):
-                    self.draw(i,j)
-                self.left = False
-            else:
-                for j in range(len(self.board[i])-1,-1,-1):
-                    self.draw(i,j)
-                self.left = True
+        # self.left = True
+        # for i in range(len(self.board)):
+        #     if self.left:
+        #         for j in range(len(self.board[i])):
+        #             self.draw(i,j)
+        #         self.left = False
+        #     else:
+        #         for j in range(len(self.board[i])-1,-1,-1):
+        #             self.draw(i,j)
+        #         self.left = True
             
-            if self.scene == self.board:
-                break
-
-    def draw(self,x,y):
-        if self.board[x,y] > 1:
-            if y == len(self.board[x])-1 or y == 0: self.end = True
-            if self.end:
-                adjacent = (x < len(self.board)-1 and self.board[x+1,y] == 1)
-                self.end = False
-            else:
-                if self.left:
-                    adjacent = (y<len(self.board[x])-1 and self.board[x,y+1] == 1) # Check if the adjacent value is 1 or not
+        #     if self.scene == self.board:
+        #         break
+        indexes = np.where(self.scene == 1)
+        indexes = list(zip(indexes[0],indexes[1]))
+        lines = []
+        for index in indexes:
+            if lines:
+                line = lines[-1]
+                if line["end"]:
+                    if self.checkAdjacent(line["end"],index):
+                        lines[-1]["end"] = index
+                    else:
+                        lines.append({"start":index,"end":None})
                 else:
-                    adjacent = (y>0 and self.board[x,y-1] == 1) # Check if the adjacent value is 1 or not
+                    lines[-1]["end"] = index
+            else:
+                lines.append({"start": index,"end":None})
 
-            self.controller.moveAt(x,y,True,adjacent)
-            self.board[x,y] = 1
+        for line in lines:
+            if line["end"]:
+                self.controller.moveAt(*line["start"],True,True)
+                self.controller.moveAt(*line["end"],True,False)
+            else:
+                self.controller.moveAt(*line["start"],True,False)
+
+
+    # def draw(self,x,y):
+    #     if self.board[x,y] > 1:
+    #         if y == len(self.board[x])-1 or y == 0: self.end = True
+    #         if self.end:
+    #             adjacent = (x < len(self.board)-1 and self.board[x+1,y] == 1)
+    #             self.end = False
+    #         else:
+    #             if self.left:
+    #                 adjacent = (y<len(self.board[x])-1 and self.board[x,y+1] == 1) # Check if the adjacent value is 1 or not
+    #             else:
+    #                 adjacent = (y>0 and self.board[x,y-1] == 1) # Check if the adjacent value is 1 or not
+
+    #         self.controller.moveAt(x,y,True,adjacent)
+    #         self.board[x,y] = 1
+
+    def checkAdjacent(self,ind1,ind2):
+        x1,y1 = ind1
+        x2,y2 = ind2
+        dis = math.sqrt((x2-x1)**2+(y2-y1)**2)
+        return (dis < 2)
