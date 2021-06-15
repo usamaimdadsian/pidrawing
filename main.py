4e652d2f1050abf3ff36806d07f4f280e9e6ac2f
usamaimdad <rreigns273@gmail.com>
2021-06-15 23:28:06 +0500

    Added camera and button functioanlity

--- main.py
+++ main.py
@@ -4,30 +4,38 @@ from Image import Image
 from Drawing import Drawing
 import matplotlib.pyplot as plt
 import RPi.GPIO as GPIO
+from picamera import PiCamera
 
 
 
 
 if __name__ == '__main__':
-    inPin = 24
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(inPin,GPIO.IN)
+    inPin = 8
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(inPin,GPIO.IN,pull_up_down = GPIO.PUD_UP)
     while True:
         value = GPIO.input(inPin)
-        if value:
+        if not value:
             print("Button pressed")
+            camera = PiCamera()
+            camera.rotation = 180
+            camera.start_preview()
+            camera.capture('t2.png')
+            camera.stop_preview()
+
+            img = cv2.imread('t2.png')
+            img_o = Image(img,'edge')
+            img = img_o.rimg
+
+            scene = img
+            board = np.zeros_like(scene)
+            d = Drawing(scene,board)
+            d.startDrawing()
             break
-    # img = cv2.imread('index.jpeg')
-    # img_o = Image(img,'edge')
-    # img = img_o.rimg
 
     # img = cv2.imread('test.png',0)
     # img[img > 127] = 150
     # img[img < 127] = 1
     # img[img == 150] = 0
 
-    # scene = img
-    # board = np.zeros_like(scene)
-    # d = Drawing(scene,board)
-    # d.startDrawing()
     GPIO.cleanup()
